35671ff769909388dddbf2f96f743bf2dfca5e76
Ben Thicmaw <benthicmaw@gmail.com>
2023-07-27 15:51:30 -0300

    mkupdate

--- scripts/generate_carddata.py
+++ scripts/generate_carddata.py
@@ -2,6 +2,8 @@ import csv
 import json
 import sys
 
+from unidecode import unidecode
+
 
 def add_type(x, type):
     x.update(type=type)
@@ -11,7 +13,7 @@ def add_type(x, type):
 if __name__ == "__main__":
     args = sys.argv[1:]
 
-    csv.register_dialect('lackey', delimiter='\t', quoting=csv.QUOTE_MINIMAL, quotechar="'")
+    csv.register_dialect('lackey', delimiter='\t', quoting=csv.QUOTE_MINIMAL, quotechar='"')
 
     with open( f"../data/{ args[0] }/catalog.json", 'r') as f:
         catalog = json.load(f)
@@ -47,11 +49,11 @@ if __name__ == "__main__":
         writer.writeheader()
         for card in cards:
             writer.writerow({
-                'Name': f"{ card['name'] }{ ', ' + card['subtitle'] if card.get('subtitle') else ''}",
+                'Name': unidecode( f"{ card['name'] }{ ', ' + card['subtitle'] if card.get('subtitle') else ''}" ),
                 'Set': card['expansion_number'],
                 'ImageFile': card['image_urls'][0]['url'].split('/')[-1].split('.')[0],
                 'Type': card.get('type'),
-                'Subtypes': json.dumps(card.get('subtypes', [])),
+                'Subtypes': unidecode(", ".join(card.get('subtypes', []))),
                 'Rarity': card.get('rarity'),
                 'Color': card.get('magic_ink_color'),
                 'Cost': card.get('ink_cost'),
@@ -59,10 +61,10 @@ if __name__ == "__main__":
                 'Strength': card.get('strength'),
                 'Willpower': card.get('willpower'),
                 'Quest': card.get('quest_value'),
-                'Abilities': json.dumps(card.get('abilities', [])),
+                'Abilities': unidecode(", ".join(card.get('abilities', []))),
                 'Identifier': card.get('card_identifier'),
-                'Rules': card.get('rules_text').replace(' ', ' '),
-                'Flavor': card.get('flavor_text').replace(' ', ' '),
+                'Rules': unidecode(card.get('rules_text').replace(' ', ' ')),
+                'Flavor': unidecode(card.get('flavor_text').replace(' ', ' ')),
                 'Number': card.get('culture_invariant_id'),
-                'Artist': card.get('author', '').replace(' ', ' ').replace('\n', '')
+                'Artist': unidecode(card.get('author', '').replace(' ', ' ').replace('\n', ''))
             })
